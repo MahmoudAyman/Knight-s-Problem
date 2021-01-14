9f5b6efd902f3581c42e6ef99335e8519e7645fb
MahmoudAyman <mahmoud.ayman07@gmail.com>
2021-01-14 03:17:54 +0200

    added function definitions

---
+++ knights_divide_and_conquer.py
@@ -0,0 +1,34 @@
+"""
+Knight's Problem divide and conquer algorithm
+"""
+
+def lookUp(n,m):
+	'''
+	takes a dimensions (n,m) and returns a solved matrix of the same size
+	returns nxm matrix
+	'''
+	pass
+
+def combine(m1, m2):
+	'''
+	takes two matrices (m1 and m2)
+	returns a matrix (m_combined) which is a combined and index-adjusted matrix
+	of m1 and m2 combined
+	'''
+	def rotate(m):
+		'''
+		takes a matrix and returns a rotated version m_rotated
+		returns: matrix 
+		'''
+		pass
+	pass
+
+
+def solveBoard(n,m):
+	'''
+	takes dimensions (n, m) and returns a knight's tour on a board of size n*m
+	returns: matrix
+	'''
+	pass
+
+
